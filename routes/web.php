ef7e173ec50bb991df2c3cc44055b0aafcaf6f8a
Moamiine <medamine.teljaoui@gmail.com>
2026-03-10 15:41:54 +0000

    created routes for login , register , tournament  and equipes pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\TeamController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/tournois', [TournamentController::class, 'index'])->name('tournois');
+Route::get('/tournois/create', [TournamentController::class, 'create'])->name('tournois.create');
+
+Route::get('/equipes', [TeamController::class, 'index'])->name('equipes.index');
+Route::get('/equipes/create', [TeamController::class, 'create'])->name('equipes.create');
