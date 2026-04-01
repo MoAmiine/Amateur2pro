7ec4db144f089683f3c65b1fa6d2b5953e948567
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-04-01 14:35:20 +0100

    edit register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,25 +33,26 @@
             <p class="text-slate-400 mt-2">Prêt à dominer l'arène ?</p>
         </div>
 
-        <form action="#" method="POST" class="space-y-6">
+        <form action="{{ route('register') }}" method="POST" class="space-y-6">
+            @csrf
             <div>
                 <label class="block text-xs font-bold uppercase tracking-widest text-slate-400 mb-2">Gamertag</label>
-                <input type="text" placeholder="Ex: ProPlayer99" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
+                <input name="name" value="{{ old('name') }}" type="text" placeholder="Ex: ProPlayer99" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
             </div>
 
             <div>
                 <label class="block text-xs font-bold uppercase tracking-widest text-slate-400 mb-2">Email</label>
-                <input type="email" placeholder="joueur@email.com" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
+                <input name="email" value="{{ old('email') }}" type="email" placeholder="joueur@email.com" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
             </div>
 
             <div class="grid grid-cols-2 gap-4">
                 <div>
                     <label class="block text-xs font-bold uppercase tracking-widest text-slate-400 mb-2">Mot de passe</label>
-                    <input type="password" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
+                    <input name="password" type="password" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
                 </div>
                 <div>
                     <label class="block text-xs font-bold uppercase tracking-widest text-slate-400 mb-2">Confirmer</label>
-                    <input type="password" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
+                    <input name="password_confirmation" type="password" class="w-full bg-slate-900 border border-white/10 p-4 rounded-sm outline-none focus:border-purple-500 transition-all">
                 </div>
             </div>
 
