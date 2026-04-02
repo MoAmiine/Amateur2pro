80ec6c65ca123f8fc10c58b25f5d1be006502846
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-04-02 17:16:17 +0100

    fixed store method on tournament controller

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -6,23 +6,33 @@ use App\Models\Game;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTournamentRequest;
 use Illuminate\Support\Facades\Auth;
-
+use Symfony\Component\HttpKernel\HttpCache\Store;
+use App\Models\Tournament;
 
 class TournamentController extends Controller
 {
     public function index()
     {
-        return view('tournoi.index'); 
+        return view('tournoi.index');
     }
 
-    public function create(){
+    public function create()
+    {
         $games = Game::all();
         return view('tournoi.create', compact('games'));
     }
 
-    public function store(StoreTournamentRequest $request){
-        $validated = $request->validated();
-        Auth::user()->tournament()->create($validated);
+    public function store(StoreTournamentRequest $request)
+    {
+        Tournament::create([
+            'name' => $request->name,
+            'game_id' => $request->game_id,
+            'max_teams' => $request->max_teams,
+            'cashprize' => $request->cashprize,
+            'date' => $request->date,
+            'description' => $request->description,
+            'organizer_id' => Auth::id()
+        ]);
 
         return redirect()->route('tournois');
     }
