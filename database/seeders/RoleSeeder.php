5707b30eb0233194b8975d25e702c7689adac53f
Mohamed amine TELJAOUI <93794412+MoAmiine@users.noreply.github.com>
2026-04-12 18:33:50 +0100

    edited role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,9 +13,7 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'joueur']);
-        Role::create(['name' => 'capitaine']);
-        Role::create(['name' => 'organisateur']);
-        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'Utilisateur']);
+        Role::create(['name' => 'Administrateur']);
     }
 }
